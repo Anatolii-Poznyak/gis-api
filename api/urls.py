e406006a48dba56aff323eb133377305ef339c3d
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-04 16:45:07 +0300

    Add routes for Point to urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from api.views import PointViewSet
 
+router = DefaultRouter()
+router.register(r"points", viewset=PointViewSet, basename="point")
 
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
 
 app_name = "api"
