8986d1287c8e1f754848d6082c064e29e7148f6a
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-04 23:26:16 +0300

    Add Docker (file+compose)

--- geospot/settings.py
+++ geospot/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "rest_framework",
+    "rest_framework_gis",
     "drf_spectacular",
     "api",
 ]
@@ -75,22 +77,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "geospot.wsgi.application"
 
-GDAL_LIBRARY_PATH = "/usr/local/Cellar/gdal/3.6.4_4/lib/libgdal.dylib"
-
+# GDAL_LIBRARY_PATH = "/usr/local/Cellar/gdal/3.6.4_4/lib/libgdal.dylib"
+# GDAL_LIBRARY_PATH = os.getenv("GDAL_LIBRARY_PATH")
 # Database
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
